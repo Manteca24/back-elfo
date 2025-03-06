26e3776218a7dd501886bec0339a318a4aafe516
Agos <agozavia@gmail.com>
2025-03-06 21:51:28 +0100

    added some options to SavedPersonSchema

--- models/SavedPerson.js
+++ models/SavedPerson.js
@@ -6,7 +6,7 @@ const savedPersonSchema = new mongoose.Schema({
   name: { type: String, required: true }, // Nombre de la persona guardada
   gender: Product.obj.gender, // Hereda las opciones de género del esquema Product
   ageRange: Product.obj.ageRange, // Hereda ageRange del esquema Product
-  relation: {type: String, enum: ["madre", "padre", "hermana", "hermano", "hija", "hijo", "abuela", "abuelo", "tía", "tío", "prima", "primo", "amiga", "amigo", "sobrina", "sobrino", "pareja", "novia", "novio", "esposo", "esposa", "compañero de trabajo", "compañera de trabajo", "jefe", "jefa", "vecino", "profesor", "alumno", "alumna", "profesora", "vecina", "cliente", "mascota"], required: true },
+  relation: {type: String, enum: ["madre", "padre", "hermana", "hermano", "hija", "hijo", "abuela", "abuelo", "tía", "tío", "prima", "primo", "amiga", "amigo", "sobrina", "sobrino", "pareja", "novia", "novio", "esposo", "esposa", "cuñada", "cuñado", "compañero de trabajo", "compañera de trabajo", "jefe", "jefa", "vecino", "profesor", "alumno", "alumna", "profesora", "vecina", "cliente", "mascota"], required: true },
   filters: [
     {
       filterId: { type: mongoose.Schema.Types.ObjectId, ref: 'Filter', required: true }, // Filtro asociado
